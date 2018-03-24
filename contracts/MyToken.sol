77ea16b6b0b6f24451f3de6998c02143d01ef31b
= <liorkup@gmail.com>
2018-03-24 21:13:38 +0300

    initial commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.4.21;
+
+
+contract MyToken {
+  uint256 constant INITIAL_SUPPLY = 100;// * (10 ** uint256(decimals));
+
+  address public constant ADMIN_ADDRESS = 0x000e1cEC4Daa41D45DE7Cd92658f27e2A0A24DE3;
+
+  mapping(address => uint256) balances;
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+  uint256 public totalSupply;
+
+  string public constant name = "MyToken"; // solium-disable-line uppercase
+  string public constant symbol = "MTK"; // solium-disable-line uppercase
+  uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+  bool public transferAllowed = true;
+
+ /**
+  * @dev Constructor that gives msg.sender all of existing tokens.
+  */
+   function Test() public {
+     totalSupply = INITIAL_SUPPLY;
+     balances[ADMIN_ADDRESS] = INITIAL_SUPPLY;
+     emit Transfer(0x0, ADMIN_ADDRESS, INITIAL_SUPPLY);
+   }
+
+    /**
+     * Reviewed:
+     * - Interger overflow = OK, checked
+     */
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+      require(transferAllowed);
+      require(_to != address(0));
+      require(_value <= balances[msg.sender]);
+
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      emit Transfer(msg.sender, _to, _value);
+      return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+      require(transferAllowed);
+      require(_to != address(0));
+      require(_value <= balances[_from]);
+
+      balances[_from] -= _value;
+      balances[_to] += _value;
+      emit Transfer(_from, _to, _value);
+      return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    function preventTransfers() public returns (bool success) {
+      require(msg.sender == ADMIN_ADDRESS);
+      transferAllowed = false;
+      return true;
+    }
+
+    function allowTransfers() public returns (bool success) {
+      require(msg.sender == ADMIN_ADDRESS);
+      transferAllowed = true;
+      return true;
+    }
+
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+
+}
